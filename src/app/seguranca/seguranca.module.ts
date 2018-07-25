03700127da9cbfb6f50b6e6e21754487361faf51
Felipe Montilla <felipe.montilla.s@outlook.com>
2018-07-25 03:51:09 -0300

    Criação do HTTPInterceptor

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -10,6 +11,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { AuthGuard } from './auth-guard';
 import { LogoutService } from './logout.service';
 import { environment } from '../../environments/environment';
+import { AuthInterceptor } from './auth-interceptor';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -33,6 +35,11 @@ export function tokenGetter() {
   ],
   declarations: [LoginFormComponent],
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
     AuthGuard,
     LogoutService
   ]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   envName: 'prod',
   apiUrl: 'https://ccpeasyform-api.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('ccpeasyform-api.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('https://ccpeasyform-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
