ea688932565e42777cfbcdefdcacc813b4bcca01
Felipe Montilla <felipe.montilla.s@outlook.com>
2018-07-24 22:05:21 -0300

    Correções Deploy HML

--- src/environments/environment.hml.ts
+++ src/environments/environment.hml.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   envName: 'hml',
-  apiUrl: 'https://ccpeasyform-api.herokuapp.com/',
+  apiUrl: 'https://ccpeasyform-api.herokuapp.com',
 
   tokenWhitelistedDomains: [ new RegExp('ccpeasyform-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
