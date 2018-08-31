4df9664078beb29ea80b909d596156d38a91b495
Felipe Montilla <felipe.montilla.s@outlook.com>
2018-08-31 01:29:53 -0300

    Atualização URL PROD easyform.ccp.local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  apiUrl: 'http://easyform:8080/ccpeasyform-api',
+  apiUrl: 'http://easyform.ccp.local:8080/ccpeasyform-api',
 
-  tokenWhitelistedDomains: [ new RegExp('easyform:8080') ],
+  tokenWhitelistedDomains: [ new RegExp('easyform.ccp.local:8080') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
